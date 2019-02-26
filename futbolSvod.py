b170f394f7316b22f96ef3b36bbf4821568eb790
bagriy <bagriy@complete.ru>
2019-02-26 18:22:57 +0300

    addFutbol_good

--- futbolSvod.py
+++ futbolSvod.py
@@ -68,3 +68,21 @@ for i in range(c):
 for key in d:
 	print (key,end=':')
 	print (d[key][0],d[key][1],d[key][2],d[key][3],d[key][4])
+
+'''
+#Good from stepik
+def command(c, res):
+    if not c in dct: dct[c] = [0, 0, 0, 0, 0]
+    dct[c] = [dct[c][0] + 1, 
+                dct[c][1] + 1 if res == 3 else dct[c][1],
+                dct[c][2] + 1 if res == 1 else dct[c][2],
+                dct[c][3] + 1 if res == 0 else dct[c][3],
+                dct[c][4] + res,]  
+dct = {}
+for i in range(int(input())):
+    c1, g1, c2, g2 = input().split(';')    
+    command(c1, 3 if g1 > g2 else 1 if g1 == g2 else 0)
+    command(c2, 3 if g2 > g1 else 1 if g1 == g2 else 0)
+for c in dct:
+    print('{}:{} {} {} {} {}'.format(c, *dct[c]))
+'''
